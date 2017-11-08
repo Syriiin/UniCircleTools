c8bb32963e6e8344fd59f3b29a1d49c2bcdb9451
Samuel Cattini-Schultz <catts16@gmail.com>
2017-11-09 10:05:23 +1100

    Added simulation test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UniCircleTools;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beatmapPath = "G:\\Swin\\COS20007_OOP\\CUSTOM_PROJECT\\tests\\TRF - Survival dAnce ~no no cry more~ (Echo49) [Insane].osu";
+            string replayPath = "G:\\Swin\\COS20007_OOP\\CUSTOM_PROJECT\\tests\\Syrin - TRF - Survival dAnce ~no no cry more~ [Insane] (2017-01-31) Osu.osr";
+
+            Beatmap beatmap = new Beatmap(beatmapPath);
+            Replay replay = new Replay(replayPath);
+
+            beatmap.SimulatePlay(replay);
+
+            Console.ReadKey();
+        }
+    }
+}
